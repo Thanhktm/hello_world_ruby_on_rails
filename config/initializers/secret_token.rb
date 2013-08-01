80786fb77ad065c5ebf7d17bb540dfc2a508b295
Thanh <thanhnd.ltt@gmail.com>
2013-08-01 16:54:05 +0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Hellowordl::Application.config.secret_key_base = '1b771f2e9c48b4daf33c70b8abfa59761c35d93f46d3b2f965a7e317557dac542920aaa1acb9bf4900b36cb8c6e1790cca4becc1dadc628e63b9eece75ba22f7'
---
+++ config/routes.rb
@@ -0,0 +1,4 @@
+Hellowordl::Application.routes.draw do
+  resources :posts
+  root to: "welcome#index"
+end
